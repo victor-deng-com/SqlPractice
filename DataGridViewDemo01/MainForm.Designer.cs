1e398e9d73447cad363e867feeecd9fa75a396d9
Victor Deng <victor-deng@qq.com>
2017-09-27 10:10:38 +0800

    add many practice

---
+++ DataGridViewDemo01/MainForm.Designer.cs
@@ -0,0 +1,146 @@
+﻿namespace DataGridViewDemo01
+{
+    partial class MainForm
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要修改
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.itcastDbDataSet = new DataGridViewDemo01.ItcastDbDataSet();
+            this.tNewsBindingSource = new System.Windows.Forms.BindingSource(this.components);
+            this.t_NewsTableAdapter = new DataGridViewDemo01.ItcastDbDataSetTableAdapters.T_NewsTableAdapter();
+            this.idDataGridViewTextBoxColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.titleDataGridViewTextBoxColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.msgDataGridViewTextBoxColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.subDateTimeDataGridViewTextBoxColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.authorDataGridViewTextBoxColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.imagePathDataGridViewTextBoxColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.itcastDbDataSet)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.tNewsBindingSource)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.AutoGenerateColumns = false;
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.idDataGridViewTextBoxColumn,
+            this.titleDataGridViewTextBoxColumn,
+            this.msgDataGridViewTextBoxColumn,
+            this.subDateTimeDataGridViewTextBoxColumn,
+            this.authorDataGridViewTextBoxColumn,
+            this.imagePathDataGridViewTextBoxColumn});
+            this.dataGridView1.DataSource = this.tNewsBindingSource;
+            this.dataGridView1.Location = new System.Drawing.Point(0, 7);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.RowTemplate.Height = 27;
+            this.dataGridView1.Size = new System.Drawing.Size(824, 441);
+            this.dataGridView1.TabIndex = 0;
+            // 
+            // itcastDbDataSet
+            // 
+            this.itcastDbDataSet.DataSetName = "ItcastDbDataSet";
+            this.itcastDbDataSet.SchemaSerializationMode = System.Data.SchemaSerializationMode.IncludeSchema;
+            // 
+            // tNewsBindingSource
+            // 
+            this.tNewsBindingSource.DataMember = "T_News";
+            this.tNewsBindingSource.DataSource = this.itcastDbDataSet;
+            // 
+            // t_NewsTableAdapter
+            // 
+            this.t_NewsTableAdapter.ClearBeforeFill = true;
+            // 
+            // idDataGridViewTextBoxColumn
+            // 
+            this.idDataGridViewTextBoxColumn.DataPropertyName = "Id";
+            this.idDataGridViewTextBoxColumn.HeaderText = "Id";
+            this.idDataGridViewTextBoxColumn.Name = "idDataGridViewTextBoxColumn";
+            this.idDataGridViewTextBoxColumn.ReadOnly = true;
+            // 
+            // titleDataGridViewTextBoxColumn
+            // 
+            this.titleDataGridViewTextBoxColumn.DataPropertyName = "Title";
+            this.titleDataGridViewTextBoxColumn.HeaderText = "Title";
+            this.titleDataGridViewTextBoxColumn.Name = "titleDataGridViewTextBoxColumn";
+            // 
+            // msgDataGridViewTextBoxColumn
+            // 
+            this.msgDataGridViewTextBoxColumn.DataPropertyName = "Msg";
+            this.msgDataGridViewTextBoxColumn.HeaderText = "Msg";
+            this.msgDataGridViewTextBoxColumn.Name = "msgDataGridViewTextBoxColumn";
+            // 
+            // subDateTimeDataGridViewTextBoxColumn
+            // 
+            this.subDateTimeDataGridViewTextBoxColumn.DataPropertyName = "SubDateTime";
+            this.subDateTimeDataGridViewTextBoxColumn.HeaderText = "SubDateTime";
+            this.subDateTimeDataGridViewTextBoxColumn.Name = "subDateTimeDataGridViewTextBoxColumn";
+            // 
+            // authorDataGridViewTextBoxColumn
+            // 
+            this.authorDataGridViewTextBoxColumn.DataPropertyName = "Author";
+            this.authorDataGridViewTextBoxColumn.HeaderText = "Author";
+            this.authorDataGridViewTextBoxColumn.Name = "authorDataGridViewTextBoxColumn";
+            // 
+            // imagePathDataGridViewTextBoxColumn
+            // 
+            this.imagePathDataGridViewTextBoxColumn.DataPropertyName = "ImagePath";
+            this.imagePathDataGridViewTextBoxColumn.HeaderText = "ImagePath";
+            this.imagePathDataGridViewTextBoxColumn.Name = "imagePathDataGridViewTextBoxColumn";
+            // 
+            // MainForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(836, 460);
+            this.Controls.Add(this.dataGridView1);
+            this.Name = "MainForm";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.MainForm_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.itcastDbDataSet)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.tNewsBindingSource)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private ItcastDbDataSet itcastDbDataSet;
+        private System.Windows.Forms.BindingSource tNewsBindingSource;
+        private ItcastDbDataSetTableAdapters.T_NewsTableAdapter t_NewsTableAdapter;
+        private System.Windows.Forms.DataGridViewTextBoxColumn idDataGridViewTextBoxColumn;
+        private System.Windows.Forms.DataGridViewTextBoxColumn titleDataGridViewTextBoxColumn;
+        private System.Windows.Forms.DataGridViewTextBoxColumn msgDataGridViewTextBoxColumn;
+        private System.Windows.Forms.DataGridViewTextBoxColumn subDateTimeDataGridViewTextBoxColumn;
+        private System.Windows.Forms.DataGridViewTextBoxColumn authorDataGridViewTextBoxColumn;
+        private System.Windows.Forms.DataGridViewTextBoxColumn imagePathDataGridViewTextBoxColumn;
+    }
+}
+
---
+++ DataGridViewDemo01/MainForm.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace DataGridViewDemo01
+{
+    public partial class MainForm : Form
+    {
+        public MainForm()
+        {
+            InitializeComponent();
+        }
+
+        private void MainForm_Load(object sender, EventArgs e)
+        {
+            // TODO: 这行代码将数据加载到表“itcastDbDataSet.T_News”中。您可以根据需要移动或删除它。
+            this.t_NewsTableAdapter.Fill(this.itcastDbDataSet.T_News);
+
+        }
+    }
+}
